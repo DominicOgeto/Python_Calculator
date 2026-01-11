d5ed3f7b36b352b3210561316d4b0db4a108e1f7
Dominic <dominicogeto254@gmail.com>
2026-01-11 19:35:51 +0300

    Feat:First commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#Creating a basic python project
+num1= float(input("Enter a number: "))
+op=input("Enter an operator: ")
+num2=float(input("Enter another number: "))
+
+if op=="+":
+    print(num1+num2)
+elif op=="-":
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+elif op=="/":
+    print(num1/num2)
+else:
+    print("Invalid operation")
